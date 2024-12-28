f022fea92f708feaf8686d7f2b49aa6d2836c8e6
Kris Dhinal <krisdhinal@gmail.com>
2024-12-28 11:12:45 +0700

    adding 404.html template , create condition if page not found redirect automaticly to 404.html page, adding custome middleware to hendel that condition

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,5 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('login.html', views.login, name='login')
+    path('login.html', views.login, name='login'),
 ]
+
+
